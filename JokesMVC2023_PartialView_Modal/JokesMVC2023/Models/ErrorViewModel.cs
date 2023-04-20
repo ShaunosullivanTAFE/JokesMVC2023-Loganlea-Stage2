1d1dbc2095e7da6fa83ebb6787ae6023b8c83c47
shaun <shaunosullivan90@live.com>
2023-04-20 17:37:42 +1000

    Removed Duplicate Project Files

--- JokesMVC2023_PartialView_Modal/JokesMVC2023/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JokesMVC2023.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
