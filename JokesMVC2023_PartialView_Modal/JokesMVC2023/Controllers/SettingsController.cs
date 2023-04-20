d416a91cafd9dd571990cb1ccebda1a768d036d1
Shaun O'Sullivan <shaunosullivan90@live.com>
2023-04-20 15:00:40 +1000

    Add project files.

---
+++ JokesMVC2023_PartialView_Modal/JokesMVC2023/Controllers/SettingsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JokesMVC2023.Controllers
+{ 
+    /// <summary>
+    /// An alternative to having an MVC controller recieve a request and return a response.
+    /// API Controllers are better set up to handle requests containing JSOn and to respond to Async Fetch/AJAX requests
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class SettingsController : ControllerBase
+    {
+        /// <summary>
+        /// Recieves a string that describes the new theme on the client
+        /// </summary>
+        /// <remarks>
+        ///     This governs which CSS file will be returned to the client on the next response.
+        /// </remarks>
+        /// <param name="updatedTheme">either 'light' or 'dark' depending on which theme has just been set</param>
+        /// <returns></returns>
+        [HttpPost("SetTheme")]
+        public async Task<IActionResult> SetTheme([FromBody] ThemeSetting updatedTheme)
+        {
+            HttpContext.Session.SetString("theme", updatedTheme.Theme);
+            return Ok();
+        }
+    }
+
+    /// <summary>
+    /// Created to help the Framework with extracting the parameters from a fetch request from the client
+    /// </summary>
+    public class ThemeSetting
+    {
+        public string Theme { get; set; }
+    }
+}
---
+++ JokesMVC2023_PartialView_Modal/JokesMVC2023/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿
+async function switchTheme() {
+    // retrieve the currently stored theme from localstorage (this may have a value, or be undefined)
+    let currentTheme = localStorage.getItem('theme')
+
+    // if the current theme is not 'undefined' and is set to dark, then we are setting the theme to 'light'
+    if (currentTheme && currentTheme == 'light') {
+
+        // update the localStorage to set the theme to light
+        localStorage.setItem('theme', 'dark')
+
+        // Send a POST request to the server to update the session to record the change in theme
+        let result = await fetch('/api/Settings/SetTheme', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({ theme: "dark" })
+        })
+        console.log(result)
+        // perform the client-side change to the theme, by switching out the stylesheet (this is faster with caching enabled!)
+        document.getElementById('themeStyle').setAttribute('href', '/css/DarkTheme.css')
+
+    } else {
+        // setting the theme to dark
+        localStorage.setItem('theme', 'light')
+
+        // sending a POST request with a query parameter - simpler than sending a post request with Body parameters
+        let result = await fetch('/api/Settings/SetTheme', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({theme: "light"})
+        })
+        console.log(result)
+
+        document.getElementById('themeStyle').setAttribute('href', '/css/LightTheme.css')
+    }
+}
